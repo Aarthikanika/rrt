0f799c26e365f1a14387a6c68ba4164b34032fd7
Aarthikanika <aarthielangovann@gmail.com>
2016-09-15 07:47:37 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
-public class Swap
-{
-  public static void main(String[] args)
-  {
-    Scanner sc=new Scanner(System.in);
-		String str=sc.next();
-		char c;
-		char ch[]=str.toCharArray();
-		int len=str.length();
-		if(len%2==0)
-		{
-			for(int i=0;i<len;i=i+2)
-			{
-				c=ch[i];
-				ch[i]=ch[i+1];
-				ch[i+1]=c;
-			}
-		for(int i=0;i<len;i++)
-			System.out.print(ch[i]);
-		}
-		else
-			System.out.println("Can't swap");
-		sc.close();
-	}
+import java.util.Scanner;
+
+public class NewClass9 {
+        public static void main(String args[])
+    {
+     
+     char temp,a,b;
+     int output1,z;
+      String str,x="";
+        Scanner in=new Scanner(System.in);
+    str=in.nextLine();
+    char array[]=str.toCharArray();
+    char c[]=new char[str.length()];
+      for(int y=0;y<str.length()-1;y++)
+       {
+          
+      
+                    a=  array[y];
+                     b= array[y+1];
+              
+                  temp=array[y+1];
+                  c[y+1]= array[y];
+                   c[y]=temp;
+                   y=y+1;
+              }
+              
+       
+  for(int k=0;k<str.length();k++) {
+    System.out.print(c[k]);
+   
+    
 }
+  System.out.println();
+
+    }}
